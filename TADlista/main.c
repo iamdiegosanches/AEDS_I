84c604d8f30c84cf30412ba48a2853abc73d7175
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-27 13:44:14 -0300

    Update main.c

--- TADlista/main.c
+++ TADlista/main.c
@@ -1,187 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "lista.h"
-/*
-typedef struct
-{
-    int codigo;
-    char nome[100];
-    int quantidade;
-    int preco;
-} TProduto;
+#include "interface.h"
 
-typedef struct celula
-{
-    TProduto item;
-    struct celula *prox;
-} TCelula;
-
-typedef struct
-{
-    TCelula* primeiro;
-    TCelula* ultimo;
-    int tamanho;
-} TLista;
-
-void FLVazia(TLista *Lista)
-{
-    Lista->primeiro = (TCelula *)malloc(sizeof(TCelula));
-    Lista->ultimo = Lista->primeiro;
-    Lista->primeiro->prox = NULL;
-    Lista->tamanho = 0;
-}
-
-int Vazia(TLista Lista)
-{
-    return (Lista.primeiro == Lista.ultimo);
-}
-
-int Tamanho (TLista Lista)
-{
-    return Lista.tamanho;
-}
-
-void Inserir (TProduto x, TLista *Lista)
-{
-    Lista->ultimo->prox = (TCelula *)malloc(sizeof(TCelula));
-    Lista->ultimo = Lista->ultimo->prox;
-    Lista->ultimo->item = x;
-    Lista->ultimo->prox = NULL;
-    Lista->tamanho++;
-}
-
-// LerProduto e ImprimirProduto são funções auxiliares
-void LerProduto(TProduto *x)
-{
-    printf("Digite codigo do produto: ");
-    __fpurge(stdin);
-    //fflush(stdin);
-    scanf("%d", &x->codigo);
-
-    printf("Digite o nome do produto: ");
-    __fpurge(stdin);
-    //fflush(stdin);
-    fgets(x->nome,100,stdin);
-
-    printf("Digite oq quantidade: ");
-    __fpurge(stdin);
-    //fflush(stdin);
-    scanf("%d", &x->quantidade);
-
-    printf("Digite o preco: ");
-    __fpurge(stdin);
-    //fflush(stdin);
-    scanf("%d", &x->preco);
-    printf("\n");
-}
-
-void ImprimirProduto(TProduto x)
-{
-    printf("\nCodigo: %d\n", x.codigo);
-    printf("Nome: %s", x.nome);
-    printf("Quantidade: %d\n", x.quantidade);
-    printf("Preco: %d\n\n", x.preco);
-}
-
-void Imprimir(TLista lista)
-{
-    TCelula* Aux;
-    Aux = lista.primeiro->prox;
-    while(Aux != NULL)
-    {
-        ImprimirProduto(Aux->item);
-        Aux = Aux->prox;
-    }
-}
-
-int Pesquisar2 (TLista Lista, TProduto Item)
-{
-    TCelula* Aux;
-    Aux = Lista.primeiro;
-    while (Aux->prox != NULL)
-    {
-        if(Aux->prox->item.codigo == Item.codigo)
-            return 1;
-        Aux = Aux->prox;
-    }
-    return 0;
-}
-
-
-TCelula* Pesquisar (TLista Lista, TProduto Item)
-{
-    TCelula* Aux;
-    Aux = Lista.primeiro;
-    while (Aux->prox != NULL)
-    {
-        if(Aux->prox->item.codigo == Item.codigo)
-            return Aux; // Retorna o ponteiro para a célula anterior
-        Aux = Aux->prox;
-    }
-    return NULL;
-}
-
-void Excluir (TLista *Lista, TProduto *Item)
-{
-    TCelula *Aux1, *Aux2;
-    Aux1 = Pesquisar(*Lista, *Item);
-    if(Aux1 != NULL)
-    {
-        Aux2 = Aux1->prox;
-        Aux1->prox = Aux2->prox;
-        *Item = Aux2->item;
-        if(Aux1->prox == NULL)
-            Lista->ultimo = Aux1;
-        free(Aux2);
-        Lista->tamanho--;
-    }
-    else
-        Item->codigo = -1;
-}
-*/
 int main()
 {
-    TLista lista;
-    FLVazia(&lista);
-    TProduto x;
 
-    int i;
-    for (i = 0; i < 2; ++i)
-    {
-        LerProduto(&x);
-        Inserir(x, &lista);
-    }
+    TLista L1;
+    //TProduto x;
+    FLVazia(&L1);
 
-    Imprimir(lista);
+    MENU(&L1);
 
     /*
-        if(Pesquisar2(lista, x) == 1) {
-            printf("O item esta na lista!\n");
-        } else {
-            printf("O item nao esta na lista!\n");
-        }
-    */
+    for(i=0; i<2; i++)
+    {
+        LerProduto(&x);
+        Inserir(x, &L1);
 
-    printf("\nInforme o codigo do produto que deseja pesquisar: ");
+    }
+
+    Imprimir(L1);
+
+    printf("\n Informe o codigo que voce deseja saber se esta na lista: ");
     scanf("%d", &x.codigo);
 
-    if(Pesquisar(lista, x) != NULL)
-        printf("O produto esta na lista!\n");
+    if(Pesquisar(L1,x) != NULL)
+        printf("\n-> O produto esta na lista\n");
     else
-        printf("O produto NAO esta na lista!\n");
+        printf("\n-> O produto nao esta na lista\n");
 
-    printf("\nInforme o codigo do produto que deseja excluir: ");
+
+    printf("\n Informe o codigo do produto que voce deseja retirar: ");
     scanf("%d", &x.codigo);
 
-    Excluir(&lista, &x);
+
+    if(Pesquisar2(L1,x) == 1)
+        printf("\n-> O produto esta na lista\n");
+    else
+        printf("\n-> O produto nao esta na lista\n");
+
+    Excluir(&L1, &x);
+
 
     if(x.codigo != -1)
-        printf("\nItem excluido com sucesso!\n");
+        printf("\n Produto foi retirado na lista\n");
     else
-        printf("\nNao foi possivel excluir o produto!\n");
+        printf("\n Produto nao esta na lista\n");
 
-    Imprimir(lista);
+    Imprimir(L1);*/
 
     return 0;
 }
