bf4776c3f966521ea696756eb5ae458106ce85c8
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-27 22:11:16 -0300

    Update Fila.h

--- TADFila/Fila.h
+++ TADFila/Fila.h
@@ -1,1 +1,32 @@
+#ifndef HEADER_FILA
+#define HEADER_FILA
 
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct item {
+    int codigo;
+} TProduto;
+
+typedef struct celula {
+    TProduto item;
+    struct celula *prox;
+} TCelula;
+
+typedef struct {
+    TCelula *frente;
+    TCelula *tras;
+    int tamanho;
+} TFila;
+
+void FFVazia (TFila *Fila);
+int Vazia(TFila Fila);
+int Tamanho(TFila Fila);
+void Enfileirar (TProduto x, TFila *Fila);
+void Desenfileirar (TFila *Fila, TProduto *Item);
+void ImprimirProduto(TProduto x);
+void Imprimir1 (TFila *Fila1);
+void Imprimir2 (TFila *Fila);
+TProduto Pesquisar (TFila *Fila1, TProduto Item);
+
+#endif // HEADER_FILA
