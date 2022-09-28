a6417e62e6f6b68318aa6fa6af580a583130a72e
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-28 20:43:52 -0300

    Update Fila.h

--- TADFila/Fila.h
+++ TADFila/Fila.h
@@ -4,16 +4,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct item {
+typedef struct item
+{
     int codigo;
 } TProduto;
 
-typedef struct celula {
+typedef struct celula
+{
     TProduto item;
     struct celula *prox;
 } TCelula;
 
-typedef struct {
+typedef struct
+{
     TCelula *frente;
     TCelula *tras;
     int tamanho;
@@ -24,7 +27,9 @@ int Vazia(TFila Fila);
 int Tamanho(TFila Fila);
 void Enfileirar (TProduto x, TFila *Fila);
 void Desenfileirar (TFila *Fila, TProduto *Item);
+void LerProduto(TProduto *x);
 void ImprimirProduto(TProduto x);
+void Liberar (TFila *Fila);
 void Imprimir1 (TFila *Fila1);
 void Imprimir2 (TFila *Fila);
 TProduto Pesquisar (TFila *Fila1, TProduto Item);
