dd57cdae01e540d2a75d2422510e17a11c119ef9
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-29 23:58:04 -0300

    Update pilha.h

--- TADPilha/pilha.h
+++ TADPilha/pilha.h
@@ -1,1 +1,30 @@
+#ifndef HEADER_PILHA
+#define HEADER_PILHA
 
+typedef struct{
+    int codigo;
+    // Demais estruturas
+} TProduto;
+
+typedef struct celula {
+    TProduto item;
+    struct celula* prox;
+} TCelula;
+
+typedef struct {
+    TCelula *fundo;
+    TCelula *topo;
+    int tamanho;
+} TPilha;
+
+void FPVazia (TPilha *Pilha);
+int Vazia (TPilha Pilha);
+int Tamanho (TPilha Pilha);
+void Push (TPilha *Pilha, TProduto x);
+void Pop (TPilha *Pilha, TProduto *Item);
+void Liberar(TPilha *Pilha);
+void ImprimirProduto(TProduto x);
+void Imprimir (TPilha *Pilha1);
+TProduto Pesquisar (TProduto Item, TPilha *Pilha1);
+
+#endif // HEADER_PILHA
