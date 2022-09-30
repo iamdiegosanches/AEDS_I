e336ea11a6660eb2bd4e8e21e4dad8b84fdbf3c2
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-29 23:54:13 -0300

    Update pilha.c

--- TADPilha/pilha.c
+++ TADPilha/pilha.c
@@ -1,3 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pilha.h"
+
+void FPVazia (TPilha *Pilha) {
+    Pilha->topo = (TCelula *)malloc(sizeof(TCelula));
+    Pilha->fundo = Pilha->topo;
+    Pilha->topo->prox = NULL;
+    Pilha->tamanho = 0;
+}
+
+int Vazia (TPilha Pilha) {
+    return (Pilha.topo == Pilha.fundo);
+}
+
+int Tamanho (TPilha Pilha) {
+    return Pilha.tamanho;
+}
+
+void Push (TPilha *Pilha, TProduto x) {
+    TCelula *Aux;
+    Aux = (TCelula *)malloc(sizeof(TCelula));
+    Pilha->topo->item = x;
+    Aux->prox = Pilha->topo;
+    Pilha->topo = Aux;
+    Pilha->tamanho++;
+}
+
+void Pop (TPilha *Pilha, TProduto *Item) {
+    TCelula* q;
+    if (Vazia(*Pilha)) {
+        printf("Erro lista vazia \n");
+        return;
+    }
+    q = Pilha->topo;
+    Pilha->topo = q->prox;
+    *Item = q->prox->item;
+    free(q);
+    Pilha->tamanho--;
+}
+
+
 void Liberar(TPilha *Pilha) {
     TProduto x;
     while(!Vazia(*Pilha)) {
@@ -6,25 +49,29 @@ void Liberar(TPilha *Pilha) {
     free(Pilha->topo);
 }
 
+void ImprimirProduto(TProduto x) {
+    printf("\nCodigo: %d\n", x.codigo);
+}
+
 void Imprimir (TPilha *Pilha1) {
     TPilha Pilha2;
     TProduto x;
-    while (!Vazia(Pilha1)) {
+    while (!Vazia(*Pilha1)) {
         Pop(Pilha1, &x);
         ImprimirProduto(x);
         Push(&Pilha2, x);
     }
-    
-    while (!Vazia(&Pilha2)) {
-        Pop(%Pilha2, %x);
+
+    while (!Vazia(Pilha2)) {
+        Pop(&Pilha2, &x);
         Push(Pilha1, x);
     }
 }
 
-TProduto Pesquisar (TProduto Item, TPilha *Pilha) {
+TProduto Pesquisar (TProduto Item, TPilha *Pilha1) {
     TPilha Pilha2;
     TProduto x, Aux;
-    while (!Vazia(Pilha1)) {
+    while (!Vazia(*Pilha1)) {
         Pop(Pilha1, &x);
         if(Item.codigo == x.codigo)
             Aux.codigo = x.codigo;
@@ -32,9 +79,9 @@ TProduto Pesquisar (TProduto Item, TPilha *Pilha) {
             Aux.codigo = -1;
         Push(&Pilha2, x);
     }
-    
-    while (!Vazia(&Pilha2)) {
-        Pop(%Pilha2, %x);
+
+    while (!Vazia(Pilha2)) {
+        Pop(&Pilha2, &x);
         Push(Pilha1, x);
     }
     return Aux;
