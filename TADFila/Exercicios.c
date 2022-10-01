4c7703893ca6f1bba93bf1ba2b3f68b09e72ec69
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-09-30 22:59:17 -0300

    Update Exercicios.c

--- TADFila/Exercicios.c
+++ TADFila/Exercicios.c
@@ -8,8 +8,28 @@ void Liberar (TFila *Fila)
     free(Fila->frente);
 }
 
-int VerificaIgual (TFila Fila1, TFila fila2) {
+int VerificaIgual (TFila Fila1, TFila fila2) { // TERMINAR AINDA
     if (Fila1.tamanho != Fila2.tamanho) {
         return 0;
+    }
+}
+
+void ExcluirIndice (TFila *Fila1, int indice) {
+    if (indice > 0 && indice <= Tamanho(*Fila1)) {
+        int n = 1;
+        TProduto x;
+        TFila Fila2;
+        FFVazia(&Fila2);
+        while(!Vazia(*Fila1)) {
+            Desenfileirar(Fila1, &x);
+            if(n != indice)
+                Enfileirar(x, &Fila2);
+            n++;
+        }
+        while(!Vazia(Fila2)) {
+            Desenfileirar(&Fila2, &x);
+            Enfileirar(x, Fila1);
+        }
+        free(Fila2.frente);
     }
 }
