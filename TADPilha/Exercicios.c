4ade334291336cf8e50004974b67fdf74493b14f
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-10-01 11:43:25 -0300

    Update Exercicios.c

--- TADPilha/Exercicios.c
+++ TADPilha/Exercicios.c
@@ -1,3 +1,25 @@
+void InverteComPilha (TPilha *Pilha) {
+  TPilha P1, P2;
+  FPVazia(&P1);
+  FPVazia(&P2);
+  TProduto x;
+  while (!Vazia(*Pilha)) {
+    Desempilha(Pilha, &x);
+    Empilha(&P1, x);
+  }
+  while (!Vazia(P1)) {
+    Desempilha(&P1, &x);
+    Empilha(&P2, x);
+  }
+  while (!Vazia(P2)) {
+    Desempilha(&P2, &x);
+    Empilha(Pilha, x);
+  }
+  free(P1.topo);
+  free(P2.topo);
+}
+
+
 void InverteComFila (TPilha *Pilha) {
   TFila Fila;
   FFVazia(&Fila);
