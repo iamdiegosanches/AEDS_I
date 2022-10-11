d12fb5c06c17c884c575d641dd9410f96b0b4a19
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2022-10-10 22:04:55 -0300

    Update Exercicios.c

--- TADlista/Exercicios.c
+++ TADlista/Exercicios.c
@@ -31,3 +31,28 @@ void inverteLista (TLista *Lista) {
         n--;
     }
 }
+
+void sortLista (TLista *Lista) {
+    TCelula *Aux1;
+    TCelula *Aux2;
+    TCelula Aux3;
+    int n = Tamanho(*Lista);
+    int i;
+    while(n != 0) {
+        Aux1 = Lista->primeiro->prox;
+        Aux2 = Lista->primeiro->prox->prox;
+        for(i = 0; i < n-1; i++) {
+            if(Aux1->item.codigo > Aux2->item.codigo) {
+                Aux3.item = Aux1->item;
+                Aux1->item = Aux2->item;
+                Aux2->item = Aux3.item;
+                Aux1 = Aux1->prox;
+                Aux2 = Aux2->prox;
+            } else {
+                Aux1 = Aux1->prox;
+                Aux2 = Aux2->prox;
+            }
+        }
+        n--;
+    }
+}
